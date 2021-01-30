750e545d3fa72355de09a908f966f73110bbc81e
penbosso <ckbosso@st.ug.edu.gh>
2021-01-30 21:30:21 +0000

    test.cpp file

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int f() {
+  cout<<"f called";
+
+  return 1;
+}
+
+int g() {
+  cout<<"g called";
+
+  return 1;
+}
+int main() {
+
+// if(f()==1 || g()==0) {
+//   cout<<"hello";
+// }
+
+int i = 3;
+// const int *ip = &i;
+// i++ = i;
+cout<<132905/1024<<endl;
+// int *iip = &i;
+
+
+double Double;
+
+
+
+}
